486b3f439bb830edf9a79bba4f8582cc8d5bcfa3
Valter B Freitas Jr <valter191@hotmail.com>
2025-02-12 00:00:04 -0300

    feat: add PostgresCreateUserRepository

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from 'express'
 
-import { PostgresHelper } from './src/db/postgres/helper.js'
-
 const app = express()
 
 app.get('/', async (req, res) => {
-    const results = await PostgresHelper.query('SELECT * FROM users;')
+    console.log('Ok!')
 
-    console.log(results)
-
-    res.send(JSON.stringify(results))
+    res.send(200)
 })
 
 app.listen(3000, () => console.log('Listening on port 3000'))
--- src/db/postgres/migrations/01-init.sql
+++ src/db/postgres/migrations/01-init.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS users (
     ID UUID PRIMARY KEY,
     fist_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
 );
 
 --create types
