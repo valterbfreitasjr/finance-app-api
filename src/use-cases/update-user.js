614f5dbbea72b067557afe2699a723c92b6ac97d
Valter B Freitas Jr <valter191@hotmail.com>
2025-02-22 14:01:31 -0300

    feat: add depence injetion on update-user

--- src/use-cases/update-user.js
+++ src/use-cases/update-user.js
@@ -1,9 +1,11 @@
 import { EmailAlreadyInUseError } from '../errors/user.js'
 import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
 import bcrypt from 'bcrypt'
-import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
 
 export class UpdateUserUseCase {
+    constructor(postgresUpdateUserRepository) {
+        this.postgresUpdateUserRepository = postgresUpdateUserRepository
+    }
     async execute(userId, updateUserParams) {
         if (updateUserParams.email) {
             const postgresGetUserByEmailRepository =
@@ -32,9 +34,7 @@ export class UpdateUserUseCase {
             user.password = hashedPassword
         }
 
-        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
-
-        const updatedUser = await postgresUpdateUserRepository.execute(
+        const updatedUser = await this.postgresUpdateUserRepository.execute(
             userId,
             user,
         )
