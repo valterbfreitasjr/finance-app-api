814443ec24d50dcb69c7ea8c44336cde71cb2e28
Valter B Freitas Jr <valter191@hotmail.com>
2025-02-22 14:05:07 -0300

    feat: add dependence injection on all routes

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express'
-import { CreateUserController } from './src/controller/create-user.js'
 import cors from 'cors'
+
+import { PostgresGetUserByIdRepository } from './src/repositories/postgres/get-user-by-id.js'
+import { PostgresCreateUserRepository } from './src/repositories/postgres/create-user.js'
+import { PostgresUpdateUserRepository } from './src/repositories/postgres/update-user.js'
+import { PostgresDeleteUserRepository } from './src/repositories/postgres/delete-user.js'
+
+import { GetUserByIdUseCase } from './src/use-cases/get-user-by-id.js'
+import { CreateUserUseCase } from './src/use-cases/create-user.js'
+import { UpdateUserUseCase } from './src/use-cases/update-user.js'
+import { DeleteUserUseCase } from './src/use-cases/delete-user.js'
+
+import { CreateUserController } from './src/controller/create-user.js'
 import { GetUserByIdController } from './src/controller/get-user-by-id.js'
 import { UpdateUserController } from './src/controller/update-user.js'
 import { DeleteUserController } from './src/controller/delete-user.js'
@@ -19,38 +30,62 @@ app.get('/', async (req, res) => {
 
 // CREATE USER
 app.post('/api/users', async (req, res) => {
-    const createUserController = new CreateUserController()
+    const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
-    const createUserResponse = await createUserController.execute(req)
+    const createUserUseCase = new CreateUserUseCase(
+        postgresCreateUserRepository,
+    )
 
-    res.status(createUserResponse.statusCode).json(createUserResponse.body)
+    const createUserController = new CreateUserController(createUserUseCase)
+
+    const { statusCode, body } = await createUserController.execute(req)
+
+    res.status(statusCode).json(body)
 })
 
 // GET USER BY ID
 app.get('/api/users/:userId', async (req, res) => {
-    const getUserByIdController = new GetUserByIdController()
+    const postgresGetUserByIdRepository = new PostgresGetUserByIdRepository()
 
-    const getUserByIdResponse = await getUserByIdController.execute(req)
+    const getUserByIdUseCase = new GetUserByIdUseCase(
+        postgresGetUserByIdRepository,
+    )
 
-    return res.status(200).json(getUserByIdResponse.body)
+    const getUserByIdController = new GetUserByIdController(getUserByIdUseCase)
+
+    const { statusCode, body } = await getUserByIdController.execute(req)
+
+    return res.status(statusCode).json(body)
 })
 
 //PATCH USER BY ID
 app.patch('/api/users/:userId', async (req, res) => {
-    const updateUserController = new UpdateUserController()
+    const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
 
-    const updateUserResponse = await updateUserController.execute(req)
+    const updateUserUseCase = new UpdateUserUseCase(
+        postgresUpdateUserRepository,
+    )
 
-    return res.status(200).json(updateUserResponse.body)
+    const updateUserController = new UpdateUserController(updateUserUseCase)
+
+    const { statusCode, body } = await updateUserController.execute(req)
+
+    return res.status(statusCode).json(body)
 })
 
 //DELETE USER BY ID
 app.delete('/api/users/:userId', async (req, res) => {
-    const deleteUserController = new DeleteUserController()
+    const postgresDeleteUserRepository = new PostgresDeleteUserRepository()
 
-    const deleteUserResponse = await deleteUserController.execute(req)
+    const deleteUserUseCase = new DeleteUserUseCase(
+        postgresDeleteUserRepository,
+    )
 
-    return res.status(200).json(deleteUserResponse.body)
+    const deleteUserController = new DeleteUserController(deleteUserUseCase)
+
+    const { statusCode, body } = await deleteUserController.execute(req)
+
+    return res.status(statusCode).json(body)
 })
 
 app.listen(3000, () => console.log(`Listening on port ${process.env.PORT}`))
