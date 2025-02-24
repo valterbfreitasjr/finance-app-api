ee68c47ca1a9d90a8e653dd880e2a3dadd1ec609
Valter B Freitas Jr <valter191@hotmail.com>
2025-02-24 01:36:55 -0300

    feat: add PostgresCreateTransactionRepository

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -1,9 +1,12 @@
-import { PostgresHelper } from '../../db/postgres/helper.js'
+import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
         const deletedUser = await PostgresHelper.query(
-            'DELETE FROM users WHERE id = $1 RETURNING *',
+            `DELETE FROM users 
+             WHERE id = $1 
+             RETURNING *
+            `,
             [userId],
         )
         return deletedUser[0]
