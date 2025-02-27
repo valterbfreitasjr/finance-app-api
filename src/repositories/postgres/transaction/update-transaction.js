621221b23f158464a25ec8ed417cabe8b47d5a98
Valter B Freitas Jr <valter191@hotmail.com>
2025-02-27 00:50:42 -0300

    feat: add PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,33 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+            UPDATE
+                users
+            SET
+                ${updateFields.join(', ')}
+            WHERE
+                id = $${updateValues.length}
+            RETURNING
+                *
+        `
+
+        const updatedUser = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updatedUser[0]
+    }
+}
