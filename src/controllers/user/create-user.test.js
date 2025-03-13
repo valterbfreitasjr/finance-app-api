e4f3ebb19e8bcfbfea56d42908ea5ce869449b91
Valter B Freitas Jr <valter191@hotmail.com>
2025-03-13 00:16:59 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -7,6 +7,7 @@ describe('Create User Controller', () => {
         }
     }
 
+    // Create User
     it('should create an user', async () => {
         //arrange
         const createUserUseCase = new CreateUserUseCaseStub()
@@ -28,5 +29,26 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toEqual(201)
         expect(result.body).not.toBeUndefined()
         expect(result.body).not.toBeNull()
+    })
+
+    // Ensure first_name
+    it('should return 400 if first_name is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        //act
+        const httpRequest = {
+            body: {
+                last_name: 'Jr',
+                email: 'v@email.com',
+                password: '123456',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toEqual(400)
     })
 })
