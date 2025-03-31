c76729455986089c6f25a08bdf90a92dc1418e9a
Valter B Freitas Jr <valter191@hotmail.com>
2025-03-30 23:47:37 -0300

    test: ensure UpdateUserController returns 400 when email is invalid

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -38,4 +38,40 @@ describe('Update User Controller', () => {
         expect(result.statusCode).toBe(200)
         expect(result.body).not.toBeUndefined()
     })
+
+    // Invalid e-mail
+    it('should return 400 if e-mail is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                email: 'invalid-email',
+            },
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    // Invalid Password
+    it('should return 400 if password is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                password: '123',
+            },
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
