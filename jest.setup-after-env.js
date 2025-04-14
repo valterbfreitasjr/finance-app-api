4eaf90fe0c9bba94e5c7a4fe787e2d9ce78a992d
Valter B. Freitas Jr <valter191@hotmail.com>
2025-04-13 23:57:31 -0300

    fix: ensure transactions are deleted first on jest setup after env file

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,6 +1,7 @@
 import { prisma } from './prisma/prisma'
 
 beforeEach(async () => {
-    prisma.user.deleteMany({})
+    // A ordem deve ser de remoção da transaction primeiro para que seja possível a remoção do usuário
     prisma.transaction.deleteMany({})
+    prisma.user.deleteMany({})
 })
