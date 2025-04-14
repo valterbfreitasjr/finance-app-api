3c556f65af000536ec24a63976886217c1e725c1
Valter B. Freitas Jr <valter191@hotmail.com>
2025-04-14 01:50:13 -0300

    test: ensure UpdateUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/update-user.test.js
+++ src/repositories/postgres/user/update-user.test.js
@@ -10,24 +10,43 @@ describe('Update User Repository', () => {
         return { sut }
     }
 
-    it('should update user on db', async () => {
-        // arrange
-        const createdUser = await prisma.user.create({
+    const createdUser = async () => {
+        return await prisma.user.create({
             data: fakeUser,
         })
+    }
 
+    const updatedUser = {
+        ...createdUser,
+        first_name: faker.person.firstName,
+        last_name: faker.person.lastName,
+    }
+
+    it('should update user on db', async () => {
+        // arrange
         const sut = makeSut()
-
-        const updatedUser = {
-            ...createdUser,
-            first_name: faker.person.firstName,
-            last_name: faker.person.lastName,
-        }
 
         // act
         const result = await sut.execute(createdUser.id, updatedUser)
 
         // assert
         expect(result).toStrictEqual(updatedUser)
+    })
+
+    it('should call Prisma with correct params', async () => {
+        // arrange
+        const { sut } = makeSut()
+        const prismaSpy = jest.spyOn(prisma.user, 'update')
+
+        // act
+        await sut.execute(createdUser.id, updatedUser)
+
+        // assert
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: createdUser.id,
+            },
+            data: updatedUser,
+        })
     })
 })
