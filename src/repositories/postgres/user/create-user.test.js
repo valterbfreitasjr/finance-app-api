1a3539d0ff740d71c26a401aac1f3c89a5fe5116
Valter B. Freitas Jr <valter191@hotmail.com>
2025-04-13 23:02:54 -0300

    test: ensure CreateUserRepository creates a user on db

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -7,6 +7,10 @@ describe('Create User Repository', () => {
 
         const result = await sut.execute(userData)
 
-        expect(result).not.toBeNull()
+        expect(result.id).toBe(userData.id)
+        expect(result.first_name).toBe(userData.first_name)
+        expect(result.last_name).toBe(userData.last_name)
+        expect(result.email).toBe(userData.email)
+        expect(result.password).toBe(userData.password)
     })
 })
