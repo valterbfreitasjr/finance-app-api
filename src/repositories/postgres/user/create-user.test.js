74ccb203cfa56f11f9bd018c09fcdb3b6c74f4c2
Valter B. Freitas Jr <valter191@hotmail.com>
2025-04-13 23:10:39 -0300

    test: ensure CreateUserRepository call Prisma with correct params

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -1,16 +1,40 @@
 import { userData } from '../../../tests/fixtures/user'
 import { CreateUserRepository } from './create-user'
+import { prisma } from './../../../../prisma/prisma.js'
 
 describe('Create User Repository', () => {
-    it('should create a user on db successfully', async () => {
+    const makeSut = () => {
         const sut = new CreateUserRepository()
 
+        return { sut }
+    }
+
+    it('should create a user on db successfully', async () => {
+        // arrange
+        const { sut } = new makeSut()
+
+        // act
         const result = await sut.execute(userData)
 
+        // assert
         expect(result.id).toBe(userData.id)
         expect(result.first_name).toBe(userData.first_name)
         expect(result.last_name).toBe(userData.last_name)
         expect(result.email).toBe(userData.email)
         expect(result.password).toBe(userData.password)
+    })
+
+    it('should call Prisma with correct params', async () => {
+        // arrange
+        const { sut } = makeSut()
+        const prismaSpy = jest.spyOn(prisma.user, 'create')
+
+        // act
+        await sut.execute(userData)
+
+        // assert
+        expect(prismaSpy).toHaveBeenCalledWith({
+            data: userData,
+        })
     })
 })
