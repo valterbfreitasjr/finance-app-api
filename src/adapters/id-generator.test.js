22cf3b7cdb3b005473f0d9523bc508c3ae3d6279
Valter B Freitas Jr <valter191@hotmail.com>
2025-04-20 07:31:43 -0300

    test: ensure IdGenerator generates a random id

---
+++ src/adapters/id-generator.test.js
@@ -0,0 +1,22 @@
+import { IdGeneratorAdapter } from './id-generator'
+
+describe('Id Generator Adapter', () => {
+    const makeSut = () => {
+        const sut = new IdGeneratorAdapter()
+
+        return { sut }
+    }
+
+    it('should return a random id', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute()
+
+        const uuidRegex =
+            /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+        expect(result).toBeTruthy()
+        expect(typeof result).toBe('string')
+        expect(result).toMatch(uuidRegex)
+    })
+})
