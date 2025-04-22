6c5d9809d79f949c0d00379905196d7f8cc93a24
Valter B Freitas Jr <valter191@hotmail.com>
2025-04-22 00:39:48 -0300

    test: ensure /PATCH /api/users/:userId return 200 when user is updated

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest'
 import { userData as fakeUser } from '../tests'
 import { app } from '../app.js'
+import { faker } from '@faker-js/faker'
 
 describe('User Routes E2E Tests', () => {
     it('POST /users should return 201 when user is created', async () => {
@@ -14,7 +15,7 @@ describe('User Routes E2E Tests', () => {
         expect(response.status).toBe(201)
     })
 
-    it('GET /api/users/:id should return 200 when user is found', async () => {
+    it('GET /api/users/:userId should return 200 when user is found', async () => {
         // Create a user
         const createdUser = await request(app)
             .post('/api/users')
@@ -30,5 +31,33 @@ describe('User Routes E2E Tests', () => {
         expect(response.status).toBe(200)
         expect(response.body.id).toBe(createdUser.id)
         expect(response.body).toEqual(createdUser)
+    })
+
+    it('PATCH /api/users/:userId should return 200 when user is updated', async () => {
+        const createdUser = await request(app)
+            .post('/api/users')
+            .send({
+                ...fakeUser,
+                id: undefined,
+            })
+
+        const updateUserParams = {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({
+                length: 7,
+            }),
+        }
+
+        const response = await request(app)
+            .patch(`/api/users/${createdUser.body.id}`)
+            .send(updateUserParams)
+
+        expect(response.status).toBe(200)
+        expect(response.body.firstName).toBe(updateUserParams.first_name)
+        expect(response.body.lastName).toBe(updateUserParams.last_name)
+        expect(response.body.email).toBe(updateUserParams.email)
+        expect(response.body.password).not.toBe(updateUserParams.password)
     })
 })
