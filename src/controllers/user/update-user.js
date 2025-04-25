a7229edc00c607772c99a927153160b5d28c481c
Valter B Freitas Jr <valter191@hotmail.com>
2025-04-24 23:48:56 -0300

    feat: ensure UpdateUserController return 404 if UserNotFoundError is thrown

--- src/controllers/user/update-user.js
+++ src/controllers/user/update-user.js
@@ -1,10 +1,11 @@
-import { EmailAlreadyInUseError } from '../../errors/user.js'
+import { EmailAlreadyInUseError, UserNotFoundError } from '../../errors/user.js'
 import { updateUserSchema } from '../../schemas/user.js'
 import {
     checkIfUuidIsValid,
     invalidIdResponse,
     badRequest,
     ok,
+    userNotFoundResponse,
     serverError,
 } from '../helpers/index.js'
 import { ZodError } from 'zod'
@@ -40,6 +41,12 @@ export class UpdateUserController {
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message })
             }
+
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            console.error(error)
             return serverError()
         }
     }
