7971996f088a60b3b6cda01a7f6ed2e94f1f9f82
Valter B Freitas Jr <valter191@hotmail.com>
2025-04-27 23:44:58 -0300

    test: ensure GET /api/transactions?userId should return 200 when fetching transactions successfully

--- src/routes/transaction.e2e.test.js
+++ src/routes/transaction.e2e.test.js
@@ -20,4 +20,24 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.body.type).toBe(transaction.type)
         expect(response.body.amount).toBe(String(transaction.amount))
     })
+
+    it('GET /api/transaction?userId should return 200 when fetching transactions successfully', async () => {
+        const createdUser = await request(app)
+            .post('/api/users')
+            .send({
+                ...fakeUser,
+                id: undefined,
+            })
+
+        const createdTransaction = await request(app)
+            .post('/api/transactiions')
+            .send({ ...transaction, id: createdUser.id })
+
+        const response = await request(app).get(
+            `/api/transactions?userId=${createdUser.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body[0].id).toBe(createdTransaction.id)
+    })
 })
