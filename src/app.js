686c98ecd2a1bc8c2d57ee469653e7ba29aaf807
Valter B Freitas Jr <valter191@hotmail.com>
2025-05-16 15:05:01 -0300

    fix: fixed app.js and imports to ES Module

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { usersRouter, transactionsRouter } from './routes/index.js'
+
+import { swaggerUi } from 'swagger-ui-express'
+import fs from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/users', usersRouter)
+app.use('/api/transactions', transactionsRouter)
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(join(__dirname, '../docs/swagger.json'), 'utf8'),
+)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+module.export = { app }
